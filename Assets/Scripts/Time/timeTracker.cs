f403f04ace29cdb6eda42def9a1c82da34395fdc
attilatolnai <attila.tolnai@student.ehb.be>
2025-01-04 12:26:58 +0100

    Feat: added timer to game
    Added a timer that is visible when the player plays the game. Also changed the way the time spent in game is visualized. It is now shown in this format: 00:00

--- Assets/Scripts/Time/timeTracker.cs
+++ Assets/Scripts/Time/timeTracker.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class timeTracker : MonoBehaviour
 {
     public float timeSpent; // Tracks the time spent in the game
     public bool isGameRunning = true; // Indicates whether the game is active
+    public TextMeshProUGUI timerText;
 
     void Update()
     {
         if (isGameRunning)
         {
             timeSpent += Time.deltaTime; // Accumulate time while the game is running
+            UpdateTimerDisplay();
         }
     }
 
     public void ResetTime()
     {
         timeSpent = 0f; // Reset the timer when restarting the game
+        UpdateTimerDisplay();
     }
 
     public void StopTimer()
@@ -29,4 +34,23 @@ public class timeTracker : MonoBehaviour
     {
         isGameRunning = true; // Start the timer when the game begins
     }
+
+    public void UpdateTimerDisplay()
+    {
+        int minutes = Mathf.FloorToInt(timeSpent / 60);
+        int seconds = Mathf.FloorToInt(timeSpent % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public string formattedTime
+    {
+        get
+        {
+            int minutes = Mathf.FloorToInt(timeSpent / 60);
+            int seconds = Mathf.FloorToInt(timeSpent % 60);
+            return string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
+
 }
